6b0e6b51fecd7f0291ec40e918d56628407e6043
think_a_lot <39505587+think-a-lot@users.noreply.github.com>
2018-12-10 22:17:53 +0800

    017 - delete all below <footer>, add <?php get_footer(); ?> in index.php

--- themes/mvcoffee/index.php
+++ themes/mvcoffee/index.php
@@ -183,34 +183,5 @@
 
   </main>
 
-  <footer id="footer" class="footer">
 
-    <div class="footer__pagetop-btn">
-      <a href="#wrapper" onclick="scrollup(); return false;" class="img-circle text-hide">TOP</a>
-    </div>
-
-    <div class="footer__social">
-      <ul class="text-center clearfix">
-        <li><a href="https://www.facebook.com/wp.business.book" target="_blank"><img src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/btn-facebook.png" alt="Facebook"/></a></li>
-        <li><a href="#" target="_blank"><img src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/btn-twitter.png" alt="Twitter"/></a></li>
-        <li><a href="#" target="_blank"><img src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/btn-instagram.png" alt="instagram"/></a></li>
-      </ul>
-    </div>
-
-    <div class="footer__copyright">
-      <p class="text-center"><small>Copyright &copy; MaverickCoffee</small></p>
-    </div>
-
-  </footer>
-
-</div>
-
-<!--
-<script type="text/javascript" src="/js/jquery-1.11.3.min.js"></script>
-<script type="text/javascript" src="/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="/js/app.js"></script>
--->
-<?php wp_footer(); ?>  
-
-</body>
-</html>
+<?php get_footer(); ?>
