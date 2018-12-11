242dad719b8cd091c4ff37975f4cbc98f413dd8e
think_a_lot <39505587+think-a-lot@users.noreply.github.com>
2018-12-11 08:19:31 +0800

    018 - delete <title> <link>*2 in header.php / It's depends on function.php

--- themes/mvcoffee/header.php
+++ themes/mvcoffee/header.php
@@ -2,10 +2,13 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>Maverick Coffee</title>
+<!--  <title>Maverick Coffee</title>-->
+  
   <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--
   <link rel="apple-touch-icon" href="./img/touch-icon.png" />
   <link rel="shortcut icon" href="./favicon.ico" />
+-->
   
 <!--
   <link rel="stylesheet" href="./css/bootstrap-custom.css"/>
